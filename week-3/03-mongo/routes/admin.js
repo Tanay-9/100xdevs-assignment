803a2fd33888e3bf42eeca4ee85b35a3d7be6401
Tanay <tanayghoriwala001@gmail.com>
2024-05-28 23:56:08 +0530

    small validation for admin creation

--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,17 +1,46 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
     const username = req.body.username;
     const password = req.body.password;
-  
+    if(!username || !password) return res.status(403).json({
+        message : "input error"
+    })
+        const adminCreate =  await Admin.create({
+        username,
+        password
+    })
+    console.log(adminCreate);
+    res.json({
+        message : "Admin Created successfully"
+    })
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async (req, res) => {
     // Implement course creation logic
+
+    const title = req.body.title;
+    const description = req.body.description;
+    const imageLink = req.body.imageLink;
+    const price = req.body.price
+
+    //zod
+
+    const newCourse = await Course.create({
+        title,
+        description,
+        price,
+        imageLink
+    })
+    console.log(newCourse);
+    return res.json({
+        message : "Course created successfully", courseId : newCourse.id
+        })
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
