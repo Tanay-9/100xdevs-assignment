a037e4facad9b7f3185836addb0245f3e2ebaca8
Tanay <tanayghoriwala001@gmail.com>
2024-06-21 23:47:49 +0530

    added a top demo middleware

--- paytm/backend/index.js
+++ paytm/backend/index.js
@@ -7,10 +7,16 @@ const PORT = 3000;
 const mainRouter = require('./routes/index');
 
 
+const allRoute = ((req,res,next)=>{
+    console.log('yeah works everytime');
+    next();
+})
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/v1', mainRouter);
+
+
+app.use('/api/v1',allRoute,mainRouter);
 
 app.listen(PORT, () => console.log(`this is port number ${PORT}`))
 
